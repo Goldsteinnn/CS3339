c630a30c742c163e9cd746ebe5cfdecb5f58bf3f
KevinPrice <kevinpricethesecond@gmail.com>
2019-02-15 13:08:43 -0600

    changes made as of now

--- CS3339.252.Group3/Legv8Decoder.py
+++ CS3339.252.Group3/Legv8Decoder.py
@@ -1,7 +1,7 @@
 import sys
 
 opcodeStr = []  # <type 'list'>: ['Invalid Instruction', 'ADDI', 'SW', 'Invalid Instruction', 'LW', 'BLTZ', 'SLL',...]
-instrSpaced = []  #<type 'list'>: ['0 01000 00000 00001 00000 00000 001010', '1 01000 00000 00001 00000 00000 001010',...]
+instrSpaced = []  # <type 'list'>: ['0 01000 00000 00001 00000 00000 001010', '1 01000 00000 00001 00000 00000 001010',...]
 arg1 = []  # <type 'list'>: [0, 0, 0, 0, 0, 1, 1, 10, 10, 0, 3, 4, 152, 4, 10, 1, 0, 112, 0]
 arg2 = []  # <type 'list'>: [0, 1, 1, 0, 1, 0, 10, 3, 4, 5, 0, 5, 0, 5, 6, 1, 1, 0, 0]
 arg3 = []  # <type 'list'>: [0, 10, 264, 0, 264, 48, 2, 172, 216, 260, 8, 6, 0, 6, 172, -1, 264, 0, 0]
@@ -11,6 +11,8 @@ arg3Str = []  # <type 'list'>: ['', ', #10', '(R0)', '', '(R0)', '', ', #2', '(R
 mem = []  # <type 'list'>: [-1, -2, -3, 1, 2, 3, 0, 0, 5, -5, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
 binMem = []  # <type 'list'>: ['11111111111111111111111111111111', '11111111111111111111111111111110', ...]
 opcode = []  # <type> 'list'>: ['11111111111', ...]
+allInstr = [] # stores all the instructions - the memory data at the end
+numInst = 0  # number of instructions in the file
 
 rnMask = 0x3E0
 rmMask = 0x1F0000
@@ -25,8 +27,8 @@ imdataMask = 0x1FFFE0
 class Dissembler:
 
     def get_opcode(self):
-        for j in range(len(binMem)):
-            opcode.append(int(binMem[j], base=2) >> 21)
+        for j in range(len(allInstr)):
+            opcode.append(allInstr[j] >> 21)
 
     def findop(self):
         for j in range(len(opcode)):
@@ -38,8 +40,8 @@ class Dissembler:
                 arg1Str.append("R" + str(arg3[j]))
                 arg2Str.append(", R" + str(arg1[j]))
                 arg3Str.append(", R" + str(arg2[j]))
-                instrSpaced.append(str(binMem[j][0:11]) + " " + str(binMem[j][11:16]) + " " + str(binMem[j][16:22])
-                                   + " " + str(binMem[j][22:27]) + " " + str(binMem[j][27:32]))
+                instrSpaced.append(str(instructions[j][0:11]) + " " + str(instructions[j][11:16]) + " " + str(instructions[j][16:22])
+                                   + " " + str(instructions[j][22:27]) + " " + str(instructions[j][27:32]))
             elif opcode[j] == 1624:
                 opcodeStr.append("SUB")
                 arg1.append(((int(instructions[j], base=2) & rnMask) >> 5))
@@ -48,8 +50,8 @@ class Dissembler:
                 arg1Str.append("R" + str(arg3[j]))
                 arg2Str.append(", R" + str(arg1[j]))
                 arg3Str.append(", R" + str(arg2[j]))
-                instrSpaced.append(str(binMem[j][0:11]) + " " + str(binMem[j][11:16]) + " " + str(binMem[j][16:22])
-                                   + " " + str(binMem[j][22:27]) + " " + str(binMem[j][27:32]))
+                instrSpaced.append(str(instructions[j][0:11]) + " " + str(instructions[j][11:16]) + " " + str(instructions[j][16:22])
+                                   + " " + str(instructions[j][22:27]) + " " + str(instructions[j][27:32]))
 
     def print_dissembled(self):
         outFile = open(outputFileName + "_dis.txt", 'w')
@@ -69,11 +71,17 @@ for i in range(len(sys.argv)):
 # into a separate list
 
 instructions = [line.rstrip() for line in open(inputFileName, 'rb')]
+
+# read instructions until break
 for i in range(len(instructions)):
-    mem.append((int(instructions[i], base=2)))  # stores binary value for analysis
-    binMem.append(instructions[i])  # stores full value for pretty printing
+    allInstr.append((int(instructions[i], base=2)))
+    numInst += 1
+    if instructions[i] == '11111110110111101111111111100111':
+        break
 
-
+# reads everything after the break instruction
+for i in range(numInst, len(instructions)):
+    binMem.append(int(instructions[i], base=2))
 
 disassemble = Dissembler()
 disassemble.get_opcode()
